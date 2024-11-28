a42949524d377ec4f3b95cc6037d2fd4e68d0d1f
tobi martin <tobimartin@gmail.com>
2024-11-28 12:42:57 +0100

    mongodb project

---
+++ routes/userRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import db from "../db/conn.js";
+const userRoute = express.Router();
+userRoute.get("/", async (req, res) => {
+    const collection = db.collection("users")
+    const result = await collection.find({}).limit(15).toArray();
+
+
+
+
+  res.status(200).json(result);
+});
+
+export default userRoute;
